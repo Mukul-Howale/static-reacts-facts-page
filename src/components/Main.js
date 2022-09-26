0ff7394595702f8a3a7cc2a01d86a96ff4a24922
Mukul Howale <howalemukul@gmail.com>
2022-09-27 00:47:12 +0530

    Added main content with styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,33 @@
 import React from "react";
+import "../style.css";
+import bullet from "../images/Ellipse 1.png"
 
 export default function Main(){
     return (
-        <h1>Main content goes here!</h1>
+        <div className="main">
+            <p>Fun facts about React</p>
+            <div className="points">
+                <div>
+                    <img src={bullet} alt="bullet"></img>
+                    <p>Was first released in 2013</p>
+                </div>
+                <div>
+                    <img src={bullet} alt="bullet"></img>
+                    <p>Was originally created by Jordan Walke</p>
+                    </div>
+                <div>
+                    <img src={bullet} alt="bullet"></img>
+                    <p>Has well over 100K stars on GitHub</p>
+                    </div>
+                <div>
+                    <img src={bullet} alt="bullet"></img>
+                    <p>Is maintained by Facebook</p>
+                    </div>
+                <div>
+                    <img src={bullet} alt="bullet"></img>
+                    <p>Powers thousands of enterprise apps, including mobile apps</p>
+                    </div>
+            </div>
+        </div>
     )
 }
