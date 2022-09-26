2c555d6e09b5f01534aa14a929a30d59fe196dc0
Mukul Howale <howalemukul@gmail.com>
2022-09-27 00:04:20 +0530

    Created a navbar and added styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "../style.css";
+import logo from "./../images/logo192.png";
 
 export default function Navbar(){
     return (
-        <h1>Navbar goes here!</h1>
+        <header>
+            <img src={logo} alt="React-Icon"></img>
+            <h2>ReactFacts</h2>
+            <h3>React Course - Project 1</h3>
+        </header>
     )
 }
